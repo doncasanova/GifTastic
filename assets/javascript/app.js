38a6bf41633484c79ae39bd601bc17a2f0d05b31
doncasanova <ethanjuan@yahoo.com>
2018-04-27 20:10:48 -0500

    added styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,37 @@
 //create an array of topics of interest
-var topics = ["gardening", "music", "cars"]
+var topics = ["gardening", "music", "cars","dogs","guitars","folk singers"]
 var still = "still"
 var stuff;
 //using a loop create buttons
 function renderButtons() {
     $("#topic-buttons").empty();
-for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
 
-   
-    // Then dynamically generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class of movie to our button
-    a.addClass(" test p-2 m-1 ");
-    // Adding a data-attribute
-    a.attr("data-name", topics[i]);
-    // Providing the initial button text
-    a.text(topics[i]);
-    // Adding the button to the buttons-view div
-    $("#topic-buttons").append(a);
-}
+
+        // Then dynamically generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of movie to our button
+        a.addClass(" test p-2 m-1 ");
+        // Adding a data-attribute
+        a.attr("data-name", topics[i]);
+        // Providing the initial button text
+        a.text(topics[i]);
+        // Adding the button to the buttons-view div
+        $("#topic-buttons").append(a);
+    }
 }
 renderButtons()
-//----------------------------------------------------------------------------------------------------
-// changes state of gif to and from sill to animate
-    function dataState() {
 
-    $(".gif").on("click", function () {
+
+$(document).ready(function () {
+    $("body").on("click", ".gif", function () {
 
         // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
         var state = $(this).attr("data-state");
         // console.log(state + " still or animated")
         // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
+        
         // Else set src to the data-still value
         if (state === "still") {
             // $(this).attr('src', src.replace('.gif', '_anim.gif'));
@@ -44,87 +43,84 @@ renderButtons()
         }
 
     });
-    }
-    //------------------------------------------------------------------------------------------------------
-    //adds new button
+});
 
-    $("#add-topic").on("click", function(event) {
-        event.preventDefault();
-        
-        // This line grabs the input from the textbox
-        var topic = $("#topic-input").val().trim();
+//------------------------------------------------------------------------------------------------------
+//adds new button
 
-        // Adding the movie from the textbox to our array
-        topics.push(topic);
-        console.log(topics);
-        
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-        displayTopicInfo()
-        $("#topic-input").val("");
-      });
+$("#add-topic").on("click", function (event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var topic = $("#topic-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    topics.push(topic);
+    console.log(topics);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+    displayTopicInfo()
+    $("#topic-input").val("");
+});
 // function to empty out the articles
 // function clear() {
-    
-//   }
-    //------------------------------------------------------------------------------------------------------
-   
-    displayTopicInfo()
-    function displayTopicInfo() {
-        $("button").on("click", function (event) {
-          
-            var stuff = $(this).attr("data-name");
 
-            $("#gifs-appear-here").empty();
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q= " + stuff + "&api_key=qENDlgCU7sKs2b61knLcGfykWxz1Osrs&limit=10";
+//------------------------------------------------------------------------------------------------------
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
+displayTopicInfo()
+function displayTopicInfo() {
+    $("button").on("click", function (event) {
 
-                var results = response.data;
-            
-           
-                // Looping over every result item
-                for (var i = 0; i < results.length; i++) {
+        var stuff = $(this).attr("data-name");
 
-                    // Only taking action if the photo has an appropriate rating
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                        // Creating a div with the class "item"
-                        var gifDiv = $("<div class='item floating-box'>");
+        $("#gifs-appear-here").empty();
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q= " + stuff + "&api_key=qENDlgCU7sKs2b61knLcGfykWxz1Osrs&limit=10";
 
-                        // Storing the result item's rating
-                        var rating = results[i].rating;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-                        // Creating a paragraph tag with the result item's rating
-                        var p = $("<p>").text("Rating: " + rating);
+            var results = response.data;
 
-                        // Creating an image tag
-                        var topicsImage = $("<img>");
 
-                        topicsImage.addClass("gif");
-                        // Giving the image tag an src attribute of a proprty pulled off the
-                        // result item
-                        // topicsImage.attr("src", results[i].images.fixed_height.url );
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
 
-                       
-                        topicsImage.attr("src", results[i].images.fixed_height_still.url,);
-                        topicsImage.attr("data-still", results[i].images.fixed_height_still.url,);
-                        topicsImage.attr("data-animate", results[i].images.fixed_height.url, "data-state", still);
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r") {
+                    // Creating a div with the class "item"
+                    var gifDiv = $("<div class='item floating-box'>");
 
-                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                        gifDiv.append(p);
-                        gifDiv.append(topicsImage);
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
 
-                        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                        $("#gifs-appear-here").prepend(gifDiv);
-                        dataState()
-                   
-                        
-                    }
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    // Creating an image tag
+                    var topicsImage = $("<img>");
+
+                    topicsImage.addClass("gif");
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    topicsImage.attr("src", results[i].images.fixed_height_still.url, );
+                    topicsImage.attr("data-still", results[i].images.fixed_height_still.url, );
+                    topicsImage.attr("data-animate", results[i].images.fixed_height.url, "data-state", still);
+
+                    // Appending the paragraph and Image 
+                    gifDiv.append(topicsImage);
+                    gifDiv.append(p);
+
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-appear-here").prepend(gifDiv);
+                    // dataState()
+
 
                 }
-            });
+
+            }
         });
-    }
+    });
+}
